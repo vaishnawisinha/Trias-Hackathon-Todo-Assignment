b4b9a14bf7b9ac40deeefb3a7050babce9ca9ac1
vaishnawisinha <80957465+vaishnawisinha@users.noreply.github.com>
2023-10-13 14:04:21 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskList from './Components/taskList';
+
+function App() {
+  return (
+    <div className="App">
+     <TaskList/>
+    </div>
+  );
+}
+
+export default App;
